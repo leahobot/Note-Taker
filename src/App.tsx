a66f4e85b8260b1d993f200c917bd3535271c3f2
leahobot <leahobot@gmail.com>
2022-12-04 16:48:42 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <div className="">
+      <h1> 
+        Hello
+      </h1>
+    </div>
+  )
+}
+
+export default App
